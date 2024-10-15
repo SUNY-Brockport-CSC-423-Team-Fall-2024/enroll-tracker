a47498b31e8bbed179ce2b6331f0d932a00d28b3
brettabamonte <abamonteb@gmail.com>
2024-10-15 17:22:44 -0400

    Project tidying

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"enroll-tracker/internal/handlers"
+	"enroll-tracker/internal/middleware"
+	"enroll-tracker/internal/repositories"
+	"enroll-tracker/internal/services"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,10 +15,29 @@ import (
 )
 
 func main() {
+	db, err := services.ConnectToDB()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	uaRepo := repositories.CreatePostgresUserAuthenticationRepository(db)
+	studentRepo := repositories.CreatePostgresStudentRepository(db)
+	userAuthService := services.CreateUserAuthenticationService(uaRepo)
+	studentService := services.CreateNewStudentService(studentRepo, userAuthService)
+
 	stdMux := http.NewServeMux()
 	stdMux.HandleFunc("/api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Healthy!")
 		w.WriteHeader(200)
 	})
+	stdMux.HandleFunc("/api/students/create-student", handlers.CreateStudentHandler(studentService))
+	stdMux.HandleFunc("/api/students/get-student", handlers.GetStudentHandler(studentService))
+	stdMux.HandleFunc("/api/students/update-student", handlers.UpdateStudentHandler(studentService))
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{}))
+	loggingMiddleware := middleware.LoggingMiddleware(logger)
+	loggedRouter := loggingMiddleware(stdMux)
 
 	l, ok := os.LookupEnv("API_CONTAINER_PORT")
 
@@ -31,7 +55,7 @@ func main() {
 
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%d", listeningPortNumber),
-		Handler: stdMux,
+		Handler: loggedRouter,
 	}
 
 	fmt.Printf("Listening on %s...\n", server.Addr)
--- backend/go.mod
+++ backend/go.mod
@@ -2,6 +2,10 @@ module enroll-tracker
 
 go 1.22.5
 
-require golang.org/x/crypto v0.28.0
+require (
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.28.0
+	golang.org/x/tools v0.26.0
+)
 
 require golang.org/x/sys v0.26.0 // indirect
