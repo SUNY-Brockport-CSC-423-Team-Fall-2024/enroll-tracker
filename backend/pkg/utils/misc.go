8c3674966fe02c0b3394da5046d8e8e99cffbdb0
brettabamonte <abamonteb@gmail.com>
2024-10-19 02:30:34 -0400

    Add helper function to see if val is contained in array

--- backend/pkg/utils/misc.go
+++ backend/pkg/utils/misc.go
@@ -5,3 +5,12 @@ import "reflect"
 func IsZeroValueStruct(v interface{}) bool {
 	return reflect.DeepEqual(v, reflect.Zero(reflect.TypeOf(v)).Interface())
 }
+
+func Contains[T comparable](val T, arr *[]T) bool {
+	for _, v := range *arr {
+		if val == v {
+			return true
+		}
+	}
+	return false
+}
