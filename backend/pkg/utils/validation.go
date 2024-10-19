21e1ff01517046a5bad50f9ccd4e4091f06f0cc0
brettabamonte <abamonteb@gmail.com>
2024-10-19 02:31:58 -0400

    Refactor to make regex rules easier to read

--- backend/pkg/utils/validation.go
+++ backend/pkg/utils/validation.go
@@ -2,8 +2,22 @@ package utils
 
 import "regexp"
 
+var RegexRules = struct {
+	Username    string
+	Password    string
+	Email       string
+	PhoneNumber string
+	Name        string
+}{
+	Username:    `^[a-z,A-Z,0-9]{8,50}$`,
+	Password:    `^[a-zA-Z0-9!@#$%^&*()_+=\[\]{};':"\\|,.<>\/?~-]{8,30}$`,
+	Email:       `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`,
+	PhoneNumber: `^\+?(\d{1,3})?[-. (]*\d{3}[-. )]*\d{3}[-. ]*\d{4}$`,
+	Name:        `^[a-zA-Z]+([ '-][a-zA-Z]+)*$`,
+}
+
 func ValidUsername(username string) bool {
-	usernameRe := regexp.MustCompile(`^[a-z,A-Z,0-9]{8,50}$`)
+	usernameRe := regexp.MustCompile(RegexRules.Username)
 	if !usernameRe.MatchString(username) {
 		return false
 	}
@@ -11,7 +25,7 @@ func ValidUsername(username string) bool {
 }
 
 func ValidPassword(password string) bool {
-	passwordRe := regexp.MustCompile(`^[a-zA-Z0-9!@#$%^&*()_+=\[\]{};':"\\|,.<>\/?~-]{8,30}$`)
+	passwordRe := regexp.MustCompile(RegexRules.Password)
 	if !passwordRe.MatchString(password) {
 		return false
 	}
@@ -19,7 +33,7 @@ func ValidPassword(password string) bool {
 }
 
 func ValidEmail(email string) bool {
-	emailRe := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	emailRe := regexp.MustCompile(RegexRules.Email)
 	if !emailRe.MatchString(email) {
 		return false
 	}
@@ -27,7 +41,7 @@ func ValidEmail(email string) bool {
 }
 
 func ValidPhoneNumber(phoneNumber string) bool {
-	phoneNumberRe := regexp.MustCompile(`^\+?(\d{1,3})?[-. (]*\d{3}[-. )]*\d{3}[-. ]*\d{4}$`)
+	phoneNumberRe := regexp.MustCompile(RegexRules.PhoneNumber)
 	if !phoneNumberRe.MatchString(phoneNumber) {
 		return false
 	}
@@ -35,7 +49,7 @@ func ValidPhoneNumber(phoneNumber string) bool {
 }
 
 func ValidName(name string) bool {
-	nameRe := regexp.MustCompile(`^[a-zA-Z]+([ '-][a-zA-Z]+)*$`)
+	nameRe := regexp.MustCompile(RegexRules.Name)
 	if !nameRe.MatchString(name) {
 		return false
 	}
