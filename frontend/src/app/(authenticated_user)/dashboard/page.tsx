09c1e377d9b603cd0a7b1bc4922aa2bdaf5c73d9
vnyabrown <vnyabrown@gmail.com>
2024-11-13 12:24:14 -0500

    update pageSlider (nonfunctional)

--- frontend/src/app/(authenticated_user)/dashboard/page.tsx
+++ frontend/src/app/(authenticated_user)/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import styles from "./styles.module.css";
+import PageSlider from "../../../app/components/page-slider";
 
 export default function Dashboard() {
   return (
     <div className={styles.dashboard_root}>
-      
+      <PageSlider firstPage={"Test1"} secondPage={"Test2"}/>
     </div>
   );
 }
