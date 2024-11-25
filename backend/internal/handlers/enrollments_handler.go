72bb0994369c924a3d26bd624c60b8186608256b
brettabamonte <abamonteb@gmail.com>
2024-11-25 13:19:00 -0500

    Fix CORS errors for same endpoints but different methods

--- backend/internal/handlers/enrollments_handler.go
+++ backend/internal/handlers/enrollments_handler.go
@@ -7,100 +7,88 @@ import (
 	"strconv"
 )
 
-func EnrollStudentHandler(s *services.EnrollmentsService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//Set CORS
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+func EnrollStudentHandler(s *services.EnrollmentsService, w http.ResponseWriter, r *http.Request) {
+	//Set CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(200)
-			return
-		}
-		courseIDParam := r.PathValue("courseID")
-		studentIDParam := r.PathValue("studentID")
+	courseIDParam := r.PathValue("courseID")
+	studentIDParam := r.PathValue("studentID")
 
-		if courseIDParam == "" {
-			http.Error(w, "Missing course id", http.StatusBadRequest)
-			return
-		}
-		if studentIDParam == "" {
-			http.Error(w, "Missing student id", http.StatusBadRequest)
-			return
-		}
-
-		courseID, err := strconv.Atoi(courseIDParam)
-		if err != nil {
-			http.Error(w, "Course id invalid format", http.StatusBadRequest)
-			return
-		}
-		studentID, err := strconv.Atoi(studentIDParam)
-		if err != nil {
-			http.Error(w, "Course id invalid format", http.StatusBadRequest)
-			return
-		}
-
-		success, err := s.EnrollStudent(courseID, studentID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		if !success {
-			http.Error(w, "Unable to enroll student in course", http.StatusBadRequest)
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
+	if courseIDParam == "" {
+		http.Error(w, "Missing course id", http.StatusBadRequest)
+		return
 	}
+	if studentIDParam == "" {
+		http.Error(w, "Missing student id", http.StatusBadRequest)
+		return
+	}
+
+	courseID, err := strconv.Atoi(courseIDParam)
+	if err != nil {
+		http.Error(w, "Course id invalid format", http.StatusBadRequest)
+		return
+	}
+	studentID, err := strconv.Atoi(studentIDParam)
+	if err != nil {
+		http.Error(w, "Course id invalid format", http.StatusBadRequest)
+		return
+	}
+
+	success, err := s.EnrollStudent(courseID, studentID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !success {
+		http.Error(w, "Unable to enroll student in course", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
 
-func UnenrollStudentHandler(s *services.EnrollmentsService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//Set CORS
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+func UnenrollStudentHandler(s *services.EnrollmentsService, w http.ResponseWriter, r *http.Request) {
+	//Set CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(200)
-			return
-		}
-		courseIDParam := r.PathValue("courseID")
-		studentIDParam := r.PathValue("studentID")
+	courseIDParam := r.PathValue("courseID")
+	studentIDParam := r.PathValue("studentID")
 
-		if courseIDParam == "" {
-			http.Error(w, "Missing course id", http.StatusBadRequest)
-			return
-		}
-		if studentIDParam == "" {
-			http.Error(w, "Missing student id", http.StatusBadRequest)
-			return
-		}
-
-		courseID, err := strconv.Atoi(courseIDParam)
-		if err != nil {
-			http.Error(w, "Course id invalid format", http.StatusBadRequest)
-			return
-		}
-		studentID, err := strconv.Atoi(studentIDParam)
-		if err != nil {
-			http.Error(w, "Course id invalid format", http.StatusBadRequest)
-			return
-		}
-
-		success, err := s.UnenrollStudent(courseID, studentID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		if !success {
-			http.Error(w, "Unable to unenroll student in course", http.StatusBadRequest)
-			return
-		}
-
-		w.WriteHeader(http.StatusNoContent)
+	if courseIDParam == "" {
+		http.Error(w, "Missing course id", http.StatusBadRequest)
+		return
 	}
+	if studentIDParam == "" {
+		http.Error(w, "Missing student id", http.StatusBadRequest)
+		return
+	}
+
+	courseID, err := strconv.Atoi(courseIDParam)
+	if err != nil {
+		http.Error(w, "Course id invalid format", http.StatusBadRequest)
+		return
+	}
+	studentID, err := strconv.Atoi(studentIDParam)
+	if err != nil {
+		http.Error(w, "Course id invalid format", http.StatusBadRequest)
+		return
+	}
+
+	success, err := s.UnenrollStudent(courseID, studentID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !success {
+		http.Error(w, "Unable to unenroll student in course", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func GetCoursesStudentsHandler(s *services.EnrollmentsService) http.HandlerFunc {
